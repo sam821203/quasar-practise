879a03657e6da5a04e3288364f4c9fe35005b283
yuhao <sam821203@yahoo.com.tw>
2023-09-15 17:29:06 +0800

    complete: Quasar PWA section 13-137

--- quasar-project/quasar.config.js
+++ quasar-project/quasar.config.js
@@ -162,7 +162,7 @@ module.exports = configure(function (/* ctx */) {
 
     // https://v2.quasar.dev/quasar-cli/developing-pwa/configuring-pwa
     pwa: {
-      workboxMode: 'generateSW', // or 'injectManifest'
+      workboxMode: 'generateSW', // 'generateSW' or 'injectManifest'
       injectPwaMetaTags: true,
       swFilename: 'sw.js',
       manifestFilename: 'manifest.json',
--- quasar-project/src-pwa/custom-service-worker.js
+++ quasar-project/src-pwa/custom-service-worker.js
@@ -6,25 +6,31 @@
  * quasar.config.js > pwa > workboxMode is set to "injectManifest"
  */
 
-import { clientsClaim } from 'workbox-core'
-import { precacheAndRoute, cleanupOutdatedCaches, createHandlerBoundToURL } from 'workbox-precaching'
-import { registerRoute, NavigationRoute } from 'workbox-routing'
+// import { clientsClaim } from 'workbox-core'
+// import { precacheAndRoute, cleanupOutdatedCaches, createHandlerBoundToURL } from 'workbox-precaching'
+// import { registerRoute, NavigationRoute } from 'workbox-routing'
 
-self.skipWaiting()
-clientsClaim()
+// self.skipWaiting()
+// clientsClaim()
 
-// Use with precache injection
-precacheAndRoute(self.__WB_MANIFEST)
+// // Use with precache injection
+// precacheAndRoute(self.__WB_MANIFEST)
 
-cleanupOutdatedCaches()
+// cleanupOutdatedCaches()
 
-// Non-SSR fallback to index.html
-// Production SSR fallback to offline.html (except for dev)
-if (process.env.MODE !== 'ssr' || process.env.PROD) {
-  registerRoute(
-    new NavigationRoute(
-      createHandlerBoundToURL(process.env.PWA_FALLBACK_HTML),
-      { denylist: [/sw\.js$/, /workbox-(.)*\.js$/] }
-    )
-  )
-}
+// // Non-SSR fallback to index.html
+// // Production SSR fallback to offline.html (except for dev)
+// if (process.env.MODE !== 'ssr' || process.env.PROD) {
+//   registerRoute(
+//     new NavigationRoute(
+//       createHandlerBoundToURL(process.env.PWA_FALLBACK_HTML),
+//       { denylist: [/sw\.js$/, /workbox-(.)*\.js$/] }
+//     )
+//   )
+// }
+
+// dependencies
+import { precacheAndRoute } from 'workbox-precaching';
+
+// config
+precacheAndRoute(self.__WB__MANIFEST)
