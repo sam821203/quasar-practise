a18d458c863f20c2fc38104ea14758ee9cfd9e6e
SamHuang <sam821203@yahoo.com.tw>
2023-09-21 16:10:56 +0800

    chore: updating workbox did not precache issue

--- quasar-project/src-pwa/custom-service-worker.js
+++ quasar-project/src-pwa/custom-service-worker.js
@@ -5,21 +5,22 @@
  * is picked up by the build system ONLY if
  * quasar.config.js > pwa > workboxMode is set to "injectManifest"
  */
-
+// dependencies
+import { precacheAndRoute } from "workbox-precaching";
+import { registerRoute } from "workbox-routing";
+import { StaleWhileRevalidate, CacheFirst } from "workbox-strategies";
 // import { clientsClaim } from 'workbox-core'
+
 // import { precacheAndRoute, cleanupOutdatedCaches, createHandlerBoundToURL } from 'workbox-precaching'
 // import { registerRoute, NavigationRoute } from 'workbox-routing'
 
 // self.skipWaiting()
 // clientsClaim()
 
-// // Use with precache injection
-// precacheAndRoute(self.__WB_MANIFEST)
-
 // cleanupOutdatedCaches()
 
-// // Non-SSR fallback to index.html
-// // Production SSR fallback to offline.html (except for dev)
+// Non-SSR fallback to index.html
+// Production SSR fallback to offline.html (except for dev)
 // if (process.env.MODE !== 'ssr' || process.env.PROD) {
 //   registerRoute(
 //     new NavigationRoute(
@@ -29,8 +30,21 @@
 //   )
 // }
 
-// dependencies
-import { precacheAndRoute } from 'workbox-precaching';
-
 // config
-precacheAndRoute(self.__WB__MANIFEST)
+// precache injection
+precacheAndRoute(self.__WB_MANIFEST);
+// precacheAndRoute([
+//   { url: "/index.html", revision: null },
+//   // 其他資源
+// ]);
+
+// caching strategies
+registerRoute(({ url }) => {
+  console.log("url: ", url);
+  // return request.destination === 'style'
+}, new CacheFirst());
+
+registerRoute(
+  ({ url }) => url.href.startsWith("http"),
+  new StaleWhileRevalidate()
+);
